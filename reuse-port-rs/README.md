56e73f7e4bbdb300a9cde09bc14fef755a5c8017
Eval EXEC <execvy@gmail.com>
2022-10-16 12:33:57 +0800

    update

--- reuse-port-rs/README.md
+++ reuse-port-rs/README.md
@@ -1,4 +1,4 @@
-# reproduce reuse port in docker container issue
+# try reproduce reuse port issue
 
 ## usage `./reuse-port-rs [reuse port] [optional: remote address]`
 example :
@@ -17,6 +17,6 @@ docker build -t reuse-port-rs:v0 .
 In host 1: execute `docker run --rm --network host reuse-port-rs:v0 8080`
 In host 2: execute `cargo run -- 18080 [remote public ip]:8080`
 
-## reproduce the issue
+## expected
 In host 1: execute `docker run --rm -p 8080:8080 reuse-port-rs:v0 8080`
 In host 2: execute `cargo run -- 18080 [remote public ip]:8080`
--- reuse-port-rs/src/main.rs
+++ reuse-port-rs/src/main.rs
@@ -1,19 +1,25 @@
-use log::info;
+use log::{error, info};
 use socket2::{Domain, SockAddr, Socket};
 use std::io::{Read, Write};
-use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpListener};
+use std::net::{IpAddr, Shutdown, SocketAddr, TcpListener};
 use std::str::FromStr;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     simple_logger::SimpleLogger::new().env().init().unwrap();
 
+    info!(
+        "os args: {}",
+        std::env::args().collect::<Vec<_>>().join(" ")
+    );
     // get first argument
     let listen_port: u16 = std::env::args().nth(1).unwrap().parse::<u16>().unwrap();
 
-    let mut remote = Arc::new(
+    let remote = Arc::new(Mutex::new(
         std::env::args()
-            .nth(2).map(|v| v.parse::<SocketAddr>().unwrap()));
+            .nth(2)
+            .map(|v| v.parse::<SocketAddr>().unwrap()),
+    ));
 
     let listen_socket = SockAddr::from(SocketAddr::new(
         IpAddr::from_str("0.0.0.0").unwrap(),
@@ -33,38 +39,56 @@ fn main() {
     let remote1 = remote.clone();
     std::thread::spawn(move || {
         // while remote_port's value is not 0
-        while remote1.is_some() {
-            let mut s = Socket::new(Domain::IPV4, socket2::Type::STREAM, None).unwrap();
-            s.set_reuse_port(true).unwrap();
-            s.set_reuse_address(true).unwrap();
-            s.bind(&listen_socket).unwrap();
+        loop {
+            if remote.lock().unwrap().is_some() {
+                info!("remote addr is {}", remote.lock().unwrap().unwrap());
+                let mut s = Socket::new(Domain::IPV4, socket2::Type::STREAM, None).unwrap();
+                s.set_reuse_port(true).unwrap();
+                s.set_reuse_address(true).unwrap();
+                s.bind(&listen_socket).unwrap();
 
-            let remote = remote1.unwrap();
-            s.connect(&SockAddr::from(remote)).unwrap();
+                let remote_addr = remote.lock().unwrap().unwrap();
+                if let Err(err) = s.connect(&SockAddr::from(remote_addr)) {
+                    error!(
+                        "connect to remote {}, failed: {}",
+                        remote.lock().unwrap().unwrap(),
+                        err
+                    );
+                    *remote.lock().unwrap() = None;
+                    std::thread::sleep(std::time::Duration::from_secs(5));
+                    continue;
+                }
 
-            let buf = format!(
-                "your addr is {}",
-                s.peer_addr().unwrap().as_socket().unwrap().to_string()
-            );
-            info!(
-                "dialing remote: {}, my addr:{} ",
-                remote.to_string(),
-                s.local_addr().unwrap().as_socket().unwrap().to_string()
-            );
-            s.write_all(buf.as_bytes()).unwrap();
+                let buf = format!(
+                    "your addr is {}",
+                    s.peer_addr().unwrap().as_socket().unwrap().to_string()
+                );
+                info!(
+                    "dialing remote: {}, my addr:{} ",
+                    remote_addr,
+                    s.local_addr().unwrap().as_socket().unwrap().to_string()
+                );
+                s.write_all(buf.as_bytes()).unwrap();
 
-            s.shutdown(Shutdown::Both).unwrap();
-
-            std::thread::sleep(std::time::Duration::from_secs(1));
+                s.shutdown(Shutdown::Both).unwrap();
+            }
+            std::thread::sleep(std::time::Duration::from_secs(5));
         }
     });
-    while let Ok((mut stream, addr)) = listener.accept() {
-        Arc::get_mut(&mut remote)
-            .unwrap()
-            .clone_from(&Some(addr));
-        let mut buf = String::new();
-        stream.read_to_string(&mut buf).unwrap();
-        info!("read from stream({}):msg: {}", addr, buf);
-        stream.shutdown(Shutdown::Both).unwrap();
-    }
+    std::thread::spawn(move || {
+        while let Ok((mut stream, addr)) = listener.accept() {
+            let mut guard = remote1.lock().unwrap();
+            *guard = Some(addr);
+            let mut buf = String::new();
+            stream.read_to_string(&mut buf).unwrap();
+            info!("read from stream({}):msg: {}", addr, buf);
+            stream.shutdown(Shutdown::Both).unwrap();
+        }
+    });
+    let (tx, rx) = std::sync::mpsc::channel();
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
+    println!("Waiting for Ctrl-C...");
+    rx.recv().expect("Could not receive from channel.");
+    println!("Got it! Exiting...");
 }
