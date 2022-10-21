6220dbb9963e046e1d8d7d773e70368c1622c8c5
Eval EXEC <execvy@gmail.com>
2022-10-22 00:53:34 +0800

    add big queue vs yamux 1M for comparision
    Signed-off-by: Eval EXEC <execvy@gmail.com>

--- ckb-log-analyzer/README.md
+++ ckb-log-analyzer/README.md
@@ -1,15 +1,17 @@
 # CKB Log Analyze Result
 ## height and timestamp on sync progress
-![inline](img/time_height.png)
+![inline](static/time_height.png)
 ## epoch and average block size on sync progress
-![inline](img/epoch_average_block_size.png)
+![inline](static/epoch_average_block_size.png)
 ## block_size and height on sync progress
-![inline image from img/full.png](img/height_block_size.png)
+![inline image from static/full.png](static/height_block_size.png)
 ## epoch and average txs count on sync progress
-![inline](img/epoch_average_txs_count.png)
+![inline](static/epoch_average_txs_count.png)
 ## height and txs count on sync progress
-![inline](img/height_txs_count.png)
+![inline](static/height_txs_count.png)
 ## epoch and average cycles on sync progress
-![inline](img/epoch_average_cycles.png)
+![inline](static/epoch_average_cycles.png)
 ## height and cycles on sync progress
-![inline](img/height_cycles.png)
+![inline](static/height_cycles.png)
+## Red: yamux-1M window size vs Blue(big ArrayQueue)
+![inline](static/time_heihgt_big_queue_vs_yamux.png)
--- ckb-log-analyzer/src/main.rs
+++ ckb-log-analyzer/src/main.rs
@@ -1,11 +1,10 @@
-use ckb_db::RocksDB;
-use ckb_db_schema::COLUMNS;
-use ckb_store::{ChainDB, ChainStore};
-use env_logger::fmt::Color::Blue;
+use crossbeam_queue::{ArrayQueue, SegQueue};
 use itertools::Itertools;
 use plotters::prelude::*;
+use rayon::prelude::*;
 use scan_fmt::scan_fmt;
 use std::cell::RefCell;
+use std::collections::hash_map::Entry;
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::BufRead;
@@ -44,16 +43,65 @@ fn load_context(now: &Instant) -> Context {
     }
 }
 
+fn parse_log_entry(filename: &str) -> BTreeMap<u64, LogStatics> {
+    let (_verifier, blocks) = parse_log_parallel(filename);
+    let mut tree = BTreeMap::new();
+    blocks.into_iter().for_each(|v| {
+        tree.insert(
+            v.block_number,
+            LogStatics {
+                cycles: 0,
+                epoch: v.epoch,
+                epoch_block_count: 0,
+                tx_count: 0,
+                timestamp: v.timestamp,
+                block_size: 0,
+            },
+        );
+    });
+    tree
+}
+
+fn parse_log_parallel(
+    filename: &str,
+) -> (SegQueue<EntryBlockVerifier>, SegQueue<EntryBlockProcess>) {
+    let file = File::open(filename).expect("file not found");
+
+    // read file line by line
+    let reader = std::io::BufReader::new(file);
+
+    let entry_block_verifer = EntryBlockVerifier::default();
+
+    let entry_block_process = EntryBlockProcess::default();
+
+    let mut verifier = SegQueue::new();
+    let mut blocks = SegQueue::new();
+    reader
+        .lines()
+        .filter_map(Result::ok)
+        .par_bridge()
+        .for_each(|line| {
+            // if let Some(v) = entry_block_verifer
+            //     .parse_line(&line) { verifier.push(v) }
+            if let Some(v) = entry_block_process.parse_line(&line) {
+                blocks.push(v)
+            }
+        });
+
+    (verifier, blocks)
+}
+
 fn build_context(now: &Instant) -> Context {
     let p0 = thread::spawn(|| {
-        let mm0 = parse_info_level_log(
-            "/home/exec/Projects/github.com/nervosnetwork/ckb-run-log/ckb-main/data/logs/run.log",
+        let mm0 = parse_log_entry(
+            // "/home/exec/Projects/github.com/nervosnetwork/ckb-run-log/ckb-main/data/logs/run.log",
+            "/home/exec/Projects/github.com/nervosnetwork/chain/logs/sync-base-turbo.log",
         );
         mm0
     });
     let p1 = thread::spawn(|| {
-        let mm1 = parse_info_level_log(
-            "/home/exec/Projects/github.com/nervosnetwork/ckb-yamux-1M/data/logs/run.log",
+        let mm1 = parse_log_entry(
+            "/home/exec/Projects/github.com/nervosnetwork/chain/sync-big-queue/data/logs/run.log",
         );
         mm1
     });
@@ -68,40 +116,40 @@ fn build_context(now: &Instant) -> Context {
     let mut mm1 = RefCell::new(p1.join().unwrap());
     info!("parse ckb yamux log {:?}", now.elapsed());
 
-    let block_size_mm = e0.join().unwrap();
-    info!("export block size {:?}", now.elapsed());
-
-    for (height, block_size) in block_size_mm {
-        mm0.get_mut()
-            .entry(height)
-            .and_modify(|v| v.block_size = block_size);
-        mm1.get_mut()
-            .entry(height)
-            .and_modify(|v| v.block_size = block_size);
-    }
-
-    info!("fill block size {:?}", now.elapsed());
-
-    let _mm0 = mm0.clone();
-    let j0 = thread::spawn(|| {
-        let epoch_mm0 = height_to_epoch(&_mm0.into_inner());
-        epoch_mm0
-    });
-
-    let _mm1 = mm1.clone();
-    let j1 = thread::spawn(|| {
-        let epoch_mm1 = height_to_epoch(&_mm1.into_inner());
-        epoch_mm1
-    });
-    let epoch_mm0 = Arc::new(j0.join().unwrap());
-    info!("epoch mm0 {:?}", now.elapsed());
-    let epoch_mm1 = Arc::new(j1.join().unwrap());
-    info!("epoch mm1 {:?}", now.elapsed());
+    // let block_size_mm = e0.join().unwrap();
+    // info!("export block size {:?}", now.elapsed());
+    //
+    // for (height, block_size) in block_size_mm {
+    //     mm0.get_mut()
+    //         .entry(height)
+    //         .and_modify(|v| v.block_size = block_size);
+    //     mm1.get_mut()
+    //         .entry(height)
+    //         .and_modify(|v| v.block_size = block_size);
+    // }
+    //
+    // info!("fill block size {:?}", now.elapsed());
+    //
+    // let _mm0 = mm0.clone();
+    // let j0 = thread::spawn(|| {
+    //     let epoch_mm0 = height_to_epoch(&_mm0.into_inner());
+    //     epoch_mm0
+    // });
+    //
+    // let _mm1 = mm1.clone();
+    // let j1 = thread::spawn(|| {
+    //     let epoch_mm1 = height_to_epoch(&_mm1.into_inner());
+    //     epoch_mm1
+    // });
+    // let epoch_mm0 = Arc::new(j0.join().unwrap());
+    // info!("epoch mm0 {:?}", now.elapsed());
+    // let epoch_mm1 = Arc::new(j1.join().unwrap());
+    // info!("epoch mm1 {:?}", now.elapsed());
 
     let c = Context {
-        epoch_mm0,
+        epoch_mm0: Arc::new(BTreeMap::new()),
         mm0: Arc::new(mm0.into_inner()),
-        epoch_mm1: Some(epoch_mm1),
+        epoch_mm1: Some(Arc::new(BTreeMap::new())),
         mm1: Some(Arc::new(mm1.into_inner())),
     };
     c
@@ -124,47 +172,47 @@ fn main() {
         info!("draw time cost {:?}", now0.elapsed());
     }));
 
-    let ac1 = ac.clone();
-    let now1 = now.clone();
-    join_handles.push(thread::spawn(move || {
-        ac1.draw_height_block_size();
-        info!("draw height block size {:?}", now1.elapsed());
-    }));
-
-    let ac2 = ac.clone();
-    let now2 = now.clone();
-    join_handles.push(thread::spawn(move || {
-        ac2.draw_epoch_average_block_size();
-        info!("draw epoch average block size {:?}", now2.elapsed());
-    }));
-
-    let ac3 = ac.clone();
-    let now3 = now.clone();
-    join_handles.push(thread::spawn(move || {
-        ac3.draw_height_cycles();
-        info!("draw height cycles {:?}", now3.elapsed());
-    }));
-
-    let ac4 = ac.clone();
-    let now4 = now.clone();
-    join_handles.push(thread::spawn(move || {
-        ac4.draw_epoch_cycles();
-        info!("draw epoch cycles {:?}", now4.elapsed());
-    }));
-
-    let ac5 = ac.clone();
-    let now5 = now.clone();
-    join_handles.push(thread::spawn(move || {
-        ac5.draw_height_txs_count();
-        info!("draw height txs count {:?}", now5.elapsed());
-    }));
-
-    let ac6 = ac.clone();
-    let now6 = now.clone();
-    join_handles.push(thread::spawn(move || {
-        ac6.draw_epoch_average_txs_count();
-        info!("draw epoch average txs count {:?}", now6.elapsed());
-    }));
+    // let ac1 = ac.clone();
+    // let now1 = now.clone();
+    // join_handles.push(thread::spawn(move || {
+    //     ac1.draw_height_block_size();
+    //     info!("draw height block size {:?}", now1.elapsed());
+    // }));
+    //
+    // let ac2 = ac.clone();
+    // let now2 = now.clone();
+    // join_handles.push(thread::spawn(move || {
+    //     ac2.draw_epoch_average_block_size();
+    //     info!("draw epoch average block size {:?}", now2.elapsed());
+    // }));
+    //
+    // let ac3 = ac.clone();
+    // let now3 = now.clone();
+    // join_handles.push(thread::spawn(move || {
+    //     ac3.draw_height_cycles();
+    //     info!("draw height cycles {:?}", now3.elapsed());
+    // }));
+    //
+    // let ac4 = ac.clone();
+    // let now4 = now.clone();
+    // join_handles.push(thread::spawn(move || {
+    //     ac4.draw_epoch_cycles();
+    //     info!("draw epoch cycles {:?}", now4.elapsed());
+    // }));
+    //
+    // let ac5 = ac.clone();
+    // let now5 = now.clone();
+    // join_handles.push(thread::spawn(move || {
+    //     ac5.draw_height_txs_count();
+    //     info!("draw height txs count {:?}", now5.elapsed());
+    // }));
+    //
+    // let ac6 = ac.clone();
+    // let now6 = now.clone();
+    // join_handles.push(thread::spawn(move || {
+    //     ac6.draw_epoch_average_txs_count();
+    //     info!("draw epoch average txs count {:?}", now6.elapsed());
+    // }));
 
     // join join_handles
     for jh in join_handles {
@@ -369,11 +417,12 @@ impl Context {
             .map(|(k, v)| (v.timestamp, *k))
             .collect();
         let first = points.first().unwrap().0;
-        let points = points.iter().map(|(k, v)| (*k - first, *v)).collect();
+        let points: Vec<(u64, u64)> = points.iter().map(|(k, v)| (*k - first, *v)).collect();
+
         let points1: Option<Vec<(u64, u64)>> = self.mm1.as_ref().map(|mm| {
             mm.iter()
                 .filter(|(k, _)| **k % 100 == 0)
-                .map(|(k, v)| (*k, v.timestamp))
+                .map(|(k, v)| (v.timestamp, *k))
                 .collect()
         });
         let points1 = points1.map(|p| {
@@ -381,9 +430,9 @@ impl Context {
             p.iter().map(|(k, v)| (*k - first, *v)).collect()
         });
         draw_u64(
-            "img/time_height.png",
+            "img/time_height_join1.png",
             "CKB Sync Status:(timestamp, height)",
-            "timestamp",
+            "timestamp(s)",
             "height",
             points,
             points1,
@@ -419,7 +468,7 @@ fn export_block_size() -> BTreeMap<u64, u64> {
     mm
 }
 
-#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
+#[derive(Serialize, Deserialize, Clone, Copy, Debug, Default)]
 struct LogStatics {
     cycles: u64,
     epoch: u64,
@@ -464,6 +513,95 @@ fn height_to_epoch(m: &BTreeMap<u64, LogStatics>) -> BTreeMap<u64, EpochStatics>
         );
     }
     ret
+}
+
+#[derive(Default)]
+struct EntryBlockVerifier {
+    block_number: u64,
+    block_hash: String,
+    block_cycle: u64,
+    block_max_cycle: u64,
+}
+
+impl EntryBlockVerifier {
+    fn parse_line(&self, line: &str) -> Option<Self> {
+        if let Some(start_i) = line.find("[block_verifier]") {
+            let (block_number, block_hash, _, _, block_cycle, block_max_cycle) = scan_fmt!(
+                &line[start_i..],
+                "[block_verifier] block number: {}, hash: Byte32({}), size:{}/{}, cycles: {}/{}",
+                u64,
+                String,
+                u64,
+                u64,
+                u64,
+                u64
+            )
+            .unwrap();
+            return Some(EntryBlockVerifier {
+                block_number,
+                block_hash,
+                block_cycle,
+                block_max_cycle,
+            });
+        }
+        None
+    }
+}
+
+#[derive(Default)]
+struct EntryBlockProcess {
+    timestamp: u64,
+    block_number: u64,
+    epoch: u64,
+    tx_count: u64,
+    epoch_block_count: u64,
+}
+
+impl EntryBlockProcess {
+    fn parse_line(&self, line: &str) -> Option<Self> {
+        let start_i = line.find("INFO ckb_chain::chain  block: ")?;
+
+        let (
+            block_number,
+            _hash_,
+            epoch,
+            _block_idx_in_epoch,
+            block_count_in_epoch,
+            _total_difficulty,
+            txs_count,
+        ) = scan_fmt!(&line[start_i..] ,  // input string
+                            "INFO ckb_chain::chain  block: {}, hash: {}, epoch: {}({}/{}), total_diff: {x}, txs: {}",     // format
+           u64,
+                String,
+                u64,
+                u64,
+                u64,
+               String,
+                u64
+            )
+        .unwrap();
+
+        if block_number % 1000 != 0 {
+            return None;
+        }
+
+        let time_start = line.find("2022-").unwrap();
+
+        let time_str = &line[time_start..23].to_string();
+        // parse time from string
+        let time = chrono::NaiveDateTime::parse_from_str(time_str, "%Y-%m-%d %H:%M:%S%.f")
+            .unwrap_or_else(|v| {
+                panic!("parse time error: {}, text: {}", v, line);
+            });
+
+        Some(EntryBlockProcess {
+            timestamp: time.timestamp() as u64,
+            block_number,
+            epoch,
+            tx_count: txs_count,
+            epoch_block_count: block_count_in_epoch,
+        })
+    }
 }
 
 fn parse_info_level_log(log_file: &str) -> BTreeMap<u64, LogStatics> {
@@ -535,6 +673,9 @@ fn parse_info_level_log(log_file: &str) -> BTreeMap<u64, LogStatics> {
                 .unwrap_or_else(|v| {
                     panic!("parse time error: {}, text: {}", v, text);
                 });
+            if block_number % 10000 == 0 {
+                info!("parse log :block_number: {}", block_number);
+            }
 
             // find first , position in text
             log_statics
@@ -566,7 +707,7 @@ fn draw_u64(
     data0: Vec<(u64, u64)>,
     data1: Option<Vec<(u64, u64)>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new(filename, (1280, 720)).into_drawing_area();
+    let root = BitMapBackend::new(filename, (1920, 1080)).into_drawing_area();
 
     let mut y_max = data0.iter().map(|(_, v)| *v).max().unwrap();
     if data1.is_some() {
@@ -582,14 +723,22 @@ fn draw_u64(
         }
     }
 
+    let mut x_max = data0.last().unwrap().0;
+    if data1.is_some() {
+        let x_max1 = data1.as_ref().unwrap().last().unwrap().0;
+        if x_max1 > x_max {
+            x_max = x_max1;
+        }
+    }
+
     root.fill(&WHITE)?;
     root.margin(10, 10, 20, 10);
 
     let mut chart = ChartBuilder::on(&root)
-        .caption(chart_name, ("sans-serif", 50).into_font())
+        .caption(chart_name, ("sans-serif", 100).into_font())
         .x_label_area_size(50)
         .y_label_area_size(100)
-        .build_cartesian_2d(data0.first().unwrap().0..data0.last().unwrap().0, 0..y_max)?;
+        .build_cartesian_2d(data0.first().unwrap().0..x_max, 0..y_max)?;
     chart
         .configure_series_labels()
         .border_style(&BLACK)
@@ -629,7 +778,7 @@ fn draw_f64(
     data0: Vec<(f64, f64)>,
     data1: Option<Vec<(f64, f64)>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new(filename, (1280, 720)).into_drawing_area();
+    let root = BitMapBackend::new(filename, (1920, 1080)).into_drawing_area();
 
     let mut y_max = data0
         .iter()
